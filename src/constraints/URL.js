5b8c3b3ff9fa36f727786ef096fd94af326cffff
WalukaUI <luke@IZkd6g2z1p5Q>
2025-03-19 13:25:52 -0500

    edited backend URL

--- src/constraints/URL.js
+++ src/constraints/URL.js
@@ -6,13 +6,10 @@
 
 const BASE_URL =
 process.env.NODE_ENV === "development"
-?"https://myappsbackend.com"
+?"http://localhost:3000"
 :"https://myappsbackend.com"
-//:"http://ec2-3-145-46-117.us-east-2.compute.amazonaws.com"
 
 export default BASE_URL
 
-
-//"https://guarded-hamlet-25175.herokuapp.com"
 //https://doc-appointment-booking-app.onrender.com
 //https://cnc-and-docapp-webservice.onrender.com
